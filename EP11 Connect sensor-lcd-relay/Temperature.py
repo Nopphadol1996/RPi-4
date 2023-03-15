5d49ce80a0fbb476313e82abf8408fa869bb386f
Nopphadol Kank;aew <89741372+Nopphadol1996@users.noreply.github.com>
2023-03-15 11:14:23 +0700

    EP11 Connect sensor-lcd-relay

---
+++ EP11 Connect sensor-lcd-relay/Temperature.py
@@ -0,0 +1,84 @@
+import Adafruit_DHT
+import random
+import csv
+import time
+import RPi.GPIO as GPIO # การใช้งาน relay
+
+import lcdlib # EP11
+lcd = lcdlib.lcd()
+
+#### Relay ####
+
+GPIO.setmode(GPIO.BCM) # 
+GPIO.setwarnings(False)
+relay = 20 # GPIO20 ขา38
+GPIO.setup(relay,GPIO.OUT)
+
+def writetocsv(data,filename='data'):
+    with open('{}.csv'.format(filename),'a',newline='',encoding='utf-8') as file:
+        fw = csv.writer(file) # fw = file wirter 
+        fw.writerow(data)
+        
+def TurnOn(sleep=0):
+    GPIO.output(relay, False)
+    if sleep > 0:
+        time.sleep(sleep)
+        print("Relay ON")
+
+def TurnOff(sleep=0):
+    GPIO.output(relay, True)
+    if sleep > 0:
+        time.sleep(sleep)
+        print("Relay OFF")
+        
+# Turnoff when start
+TurnOff()
+
+
+class Senser:
+    """ Sensor Class for IoT """
+    def __init__(self,name='DF-101'):
+        self.name = name
+        self.type = 'DHT22'
+        self.DHT_SENSOR = Adafruit_DHT.DHT22
+        self.DHT_PIN = 21 # GPIO21 ขา 40
+        self.current_temp = 0
+        self.current_humid = 0
+    
+    def get_temp_humid(self): # ฟังก์ชั่นนี้เรียกว่า method
+        h,t = Adafruit_DHT.read_retry(self.DHT_SENSOR, self.DHT_PIN)
+        return(round(t,2),round(h,2))
+
+    def show_result(self,writecsv=False):
+        temp,humid = self.get_temp_humid()
+        self.current_temp = temp
+        self.current_humid = humid
+        
+        ## LCD ###
+        text_temp = 'Temp: {:.2f}-C'.format(temp)
+        text_Humi = 'Humi: {:.2f}%F'.format(humid)
+        lcd.lcd_display_string(text_temp,1)
+        lcd.lcd_display_string(text_Humi,2)
+        
+        print('Temperature: {:.2f}°C\nHumidity: {:.2f}%'.format(temp,humid))
+
+        if writecsv == True:
+            data = [temp,humid]
+            writetocsv(data, self.name)
+
+temp1 = Senser('DHT-101')
+
+while True:
+    try:
+        temp1.show_result(True)
+        if temp1.current_temp >= 35:
+            TurnOn()
+        else:
+            TurnOff()
+    except:
+        pass
+   # print('-----')
+    time.sleep(2)
+    
+
+
